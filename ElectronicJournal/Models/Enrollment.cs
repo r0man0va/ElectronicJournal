34b8c36a3b8f7310f773113ef9f99780f5b63219
Tanya <darkoracle1@mail.ru>
2021-12-23 21:49:23 +0300

    Этап 1: Добавлена начальная предметная область БД, переведены страницы на русский.

---
+++ ElectronicJournal/Models/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicJournal.Models
+{
+    public enum Grade
+    {
+        Bad = 3,
+        Good,
+        Excellent
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "Нет оценки")]
+        public Grade? Grade { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ ElectronicJournal/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElectronicJournal.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Display(Name="Фамилия")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Группа")]
+        public string GroupName { get; set;  }
+       
+        [Display(Name = "Дата рождения")]
+        public DateTime DOB { get; set; }
+
+        [Display(Name = "Номер телефона")]
+        public string PhoneNumber {get; set; }
+
+        [Display(Name = "Дата зачисления")]
+        public DateTime EnrollmentDate { get; set; }  // дата зачисления на курс
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
